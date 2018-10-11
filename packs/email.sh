d58bae2b314b3246a6972f165a15ecef19a583b4
Tunisian Eagles <43474676+TunisianEagles@users.noreply.github.com>
2018-10-11 13:00:56 +0100

    Create email.sh

---
+++ packs/email.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#Colors as well
+white="\033[1;37m"
+grey="\033[0;37m"
+purple="\033[0;35m"
+red="\033[1;31m"
+green="\033[1;32m"
+yellow="\033[1;33m"
+purple="\033[0;35m"
+cyan="\033[0;36m"
+cafe="\033[0;33m"
+fiuscha="\033[0;35m"
+blue="\033[1;34m"
+nc="\e[0m"
+#
+api_key=777f13a9c3608015df19bbe4a8cfdd21
+read -p "        $(echo -e $red[$green+$red]$nc) $(echo -e $white Enter Your Email : $nc )" email
+curl "http://apilayer.net/api/check?access_key=$api_key&email=$email&smtp=1&format=1"
+#EOF
