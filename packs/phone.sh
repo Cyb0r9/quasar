68ab5ff4407d908fcef209c91c34e956ec919c15
Tunisian Eagles <43474676+TunisianEagles@users.noreply.github.com>
2018-10-11 13:57:30 +0100

    Update phone.sh

--- packs/phone.sh
+++ packs/phone.sh
@@ -1,6 +1,19 @@
-#!/bin//bash
+#!/bin/bash
+#Colors as well
+white="\033[1;37m"
+grey="\033[0;37m"
+purple="\033[0;35m"
+red="\033[1;31m"
+green="\033[1;32m"
+yellow="\033[1;33m"
+purple="\033[0;35m"
+cyan="\033[0;36m"
+cafe="\033[0;33m"
+fiuscha="\033[0;35m"
+blue="\033[1;34m"
+nc="\e[0m"
 api_key=ee392377bdcf8e0008cc8f0dbd40edb0
-read -p "[+] Enter Country Code EX : USA : " co
-read -p "[+] Enter Phone Number : " number
+read -p "	$(echo -e $red[$green+$red]$white Enter Country Code ex TN : $nc)" co
+read -p "	$(echo -e $red[$green+$red]$white Enter Phone Number : $nc)" number
 curl "http://apilayer.net/api/validate?access_key=$api_key&number=$number&country_code=$co&format=1"
 #EOF
