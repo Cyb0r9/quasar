f6812b32eccc863094d2f9298639cf6b9b96b088
Tunisian Eagles <43474676+TunisianEagles@users.noreply.github.com>
2018-10-11 12:56:30 +0100

    Delete phone.sh

--- packs/phone.sh
+++
@@ -1,5 +0,0 @@
-#!/bin//bash
-api_key=ee392377bdcf8e0008cc8f0dbd40edb0
-read -p "[+] Enter Country Code EX : USA : " co
-read -p "[+] Enter Phone Number : " number
-curl "http://apilayer.net/api/validate?access_key=$api_key&number=$number&country_code=$co&format=1"
