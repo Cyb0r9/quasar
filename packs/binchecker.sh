b5c80e735160d3c6e5f3d62a5f6f10fe9c1149e8
Tunisian Eagles <43474676+TunisianEagles@users.noreply.github.com>
2018-10-11 13:08:24 +0100

    Update binchecker.sh

--- packs/binchecker.sh
+++ packs/binchecker.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
-read -p "[+] Enter First 6 Digits Of A Credit Card Number : " card
+#Colors As Well
+white="\033[1;37m"
+grey="\033[0;37m"
+purple="\033[0;35m"
+red="\033[1;31m"
+green="\033[1;32m"
+yellow="\033[1;33m"
+purple="\033[0;35m"
+cyan="\033[0;36m"
+cafe="\033[0;33m"
+fiuscha="\033[0;35m"
+blue="\033[1;34m"
+nc="\e[0m"
+read -p "	$(echo -e $red[$green+$red]$nc) $(echo -e $white Enter First 6 Digits of CC : ) " card
 http https://lookup.binlist.net/$card
