84a4453b9e221cfffbe06013989e727a275b31b0
Tunisian Eagles <43474676+TunisianEagles@users.noreply.github.com>
2018-10-12 21:45:31 +0100

    Update email.sh

--- packs/email.sh
+++ packs/email.sh
@@ -12,7 +12,7 @@ cafe="\033[0;33m"
 fiuscha="\033[0;35m"
 blue="\033[1;34m"
 nc="\e[0m"
-api_key=777f13a9c3608015df19bbe4a8cfdd21
+api_key=1f34e16c74b097e47556c9a9ce5cd0dd
 read -p "        $(echo -e $red[$green+$red]$nc) $(echo -e $white Enter Your Email : $nc )" email
 curl "http://apilayer.net/api/check?access_key=$api_key&email=$email&smtp=1&format=1"
 #EOF
