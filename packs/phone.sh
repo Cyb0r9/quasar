6671e40202a815c497fffebdb557ce4d117a5803
Tunisian Eagles <bahcane-2@hotmail.fr>
2018-10-23 20:27:14 +0100

    Update phone.sh

--- packs/phone.sh
+++ packs/phone.sh
@@ -12,7 +12,7 @@ cafe="\033[0;33m"
 fiuscha="\033[0;35m"
 blue="\033[1;34m"
 nc="\e[0m"
-api_key=8cfa1112b237a5ae957da2312f17f6e0
+api_key=806e8db910e4a9e8f22415e0506debd0
 read -p "	$(echo -e $red[$green+$red]$white Enter Country Code ex TN : $nc)" co
 read -p "	$(echo -e $red[$green+$red]$white Enter Phone Number : $nc)" number
 curl "http://apilayer.net/api/validate?access_key=$api_key&number=$number&country_code=$co&format=1"
